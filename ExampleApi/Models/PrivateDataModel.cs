8e77c09065ca49eec667c3c62baae660b6aa6346
Ted Spence <ted@spence.net>
2022-11-23 09:39:27 -0800

    Improve the demo app
    The demo app can now show off security successes and failures.

---
+++ ExampleApi/Models/PrivateDataModel.cs
@@ -0,0 +1,15 @@
+﻿using SecurityBlanket.Interfaces;
+
+namespace ExampleApi.Models
+{
+    public class PrivateDataModel : ICustomSecurity
+    {
+        public int AccountId { get; set; }
+        public string Name { get { return "The name of the account"; } }
+
+        public bool IsVisible(HttpContext context)
+        {
+            return context.Items["AccountId"] as Int32? == AccountId;
+        }
+    }
+}
---
+++ ExampleApi/Models/PublicDataModel.cs
@@ -0,0 +1,12 @@
+﻿using SecurityBlanket.Interfaces;
+
+namespace ExampleApi.Models
+{
+    /// <summary>
+    /// Represents public data that isn't sensitive
+    /// </summary>
+    public class PublicDataModel : INoSecurity
+    {
+        public string News { get { return "Here is where we would place today's news"; } }
+    }
+}
